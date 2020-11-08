4647e27ebcf125b77ec04c376915415943778b61
nobasksio <a420673@yandex.ru>
2020-11-08 14:14:08 +0800

    fix format

--- lesson1/hello.js
+++ lesson1/hello.js
@@ -1,16 +1,16 @@
 var myName = prompt('Как Вас зовут?');
 var myLastName = prompt('Также, пожалуйста, укажите фамилию');
-var myBirthYear =  prompt('Введите Ваш год рождения');
+var myBirthYear = prompt('Введите Ваш год рождения');
 
-var  currentYear = 2020;
+var currentYear = 2020;
 var age = currentYear - myBirthYear;
 
 if (age < 20) {
-  alert('Привет, ' + myName + ' ' + myLastName +'!' +  ' ' + "\u{1F44B}");
+  alert('Привет, ' + myName + ' ' + myLastName +'!' + ' ' + "\u{1F44B}");
 }
-else if (age >=20 && age <40) {
-  alert('Добрый день, ' + myName  + ' ' + myLastName + ' ' + "\u{1F590}");
+else if (age >= 20 && age < 40) {
+  alert('Добрый день, ' + myName + ' ' + myLastName + ' ' + "\u{1F590}");
 }
 else {
-  alert('Здравствуйте, ' + myName + ' ' + myLastName + ' ' +  "\u{1F590}")
+  alert('Здравствуйте, ' + myName + ' ' + myLastName + ' ' + "\u{1F590}");
 }
